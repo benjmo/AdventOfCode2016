4cc2bbd8385b9a9337779cabd609bb97f5fb5405
Benjamin Mo <b.mo96@hotmail.com>
2017-01-21 21:15:46 +1100

    day06

---
+++ day06/star11.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+import hashlib
+import re
+
+def main():
+   with open('./input.txt', 'r') as file:
+      lines = file.readlines()
+
+   freqCounts = [{}, {}, {}, {}, {}, {}, {}, {}]
+   for line in lines:
+      for i in range(0, 8):
+         if (line[i] in freqCounts[i]):
+            freqCounts[i][line[i]] += 1
+         else:
+            freqCounts[i][line[i]] = 1
+
+   message = ""
+   for freqCount in freqCounts:
+      sortedFreqCount = sorted(freqCount.items(), key=lambda x: x[1], reverse=True)
+      message += sortedFreqCount[0][0]
+
+   print(message)
+
+
+
+
+if __name__ == '__main__':
+   main()
