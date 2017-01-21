cf30aeed81578c8cce32c92d25764453c36722b7
Benjamin Mo <b.mo96@hotmail.com>
2017-01-21 23:23:17 +1100

    day07 init

---
+++ day07/star13.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def main():
+   with open('./input.txt', 'r') as file:
+      lines = file.readlines()
+
+   for line in lines:
+      inside = False
+      if (inside):
+      else:
+            
+
+
+
+
+if __name__ == '__main__':
+   main()
